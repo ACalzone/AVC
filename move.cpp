c304af2cf6daa59b4918ad9b1b0ca2952d6c6298
ACalzone <callumsutikno@gmail.com>
2017-05-09 21:10:27 +1200

    Allows the robot to move
    Currently can only go forward

---
+++ move.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+#include "move.h"
+
+void move(double speed){
+	init();
+	set_motor(1, 255 * speed);
+	sleep1(0,500000);
+	set_motor(2, 255 * speed);
+	sleep1(0, 500000);
+	return 0;
+}
