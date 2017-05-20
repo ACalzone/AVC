51f9ecfd4d38ca3669561092e31dd433479e5a3e
ACalzone <callumsutikno@gmail.com>
2017-05-20 16:18:46 +1200

    Added it again
    To reference in the report if needed,

---
+++ Code/movement.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ #include <time.h>
+ #include "E101.h"
+ #include "movement.h"
+
+void move(double speedRight, double speedLeft){
+     init();
+     set_motor(1, speedRight);
+ 	   sleep1(0,500000);
+ 	   set_motor(2, speedLeft);
+     sleep1(0, 500000);
+ }
+ //This file obsolete and will not be used
