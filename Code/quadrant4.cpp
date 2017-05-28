7006f6390501e39b9aa17aa355df4213560ced69
ACalzone <callumsutikno@gmail.com>
2017-05-28 15:06:45 +1200

    Added the code

--- Code/quadrant4.cpp
+++ Code/quadrant4.cpp
@@ -3,5 +3,20 @@
 #include "E101.h"
 
 int main {
-
+    init();
+    int sensor = 0; //variable for holding the sensor values
+    int red = 1; //variable to determine if the robot has reached that red part so that the first while loop will break
+    while(red = 1){
+         sensor = read_analog(0);
+         if(sensor >threshold) { //threshold is a placeholder for when we learn what the sensor values
+           set_motor(1, 30); //should test out different speeds
+           set_motor(2, 30);
+         }
+        else {
+           stop(0);
+           sleep1(3, 0); //so it can wait for the gate to open
+           set_motor(1, 30); //not sure which motor will make it turn right
+        }
+    }
+    return 0;
 }
