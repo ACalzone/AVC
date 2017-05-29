421141e115d1bdfaf64ab9ad4988afb103577e8d
ACalzone <callumsutikno@gmail.com>
2017-05-29 16:30:32 +1200

    Updated
    It can now turn left or right using two sensors

--- Code/quadrant4.cpp
+++ Code/quadrant4.cpp
@@ -5,17 +5,36 @@
 int main {
     init();
     int sensor = 0; //variable for holding the sensor values
+    int sensor_right = 0; //variable for the right sensor
     int red = 1; //variable to determine if the robot has reached that red part so that the first while loop will break
-    while(red = 1){
+    int threshold_wall = 0;
+    int threshold_turn = 0;
+    while(red == 1){
          sensor = read_analog(0);
-         if(sensor >threshold) { //threshold is a placeholder for when we learn what the sensor values
+         if(sensor > threshold_wall) { //threshold is a placeholder for when we learn what the sensor values
            set_motor(1, 30); //should test out different speeds
            set_motor(2, 30);
          }
         else {
-           stop(0);
+           set_motor(1, 0);
+           set_motor(2, 0);
            sleep1(3, 0); //so it can wait for the gate to open
-           set_motor(1, 30); //not sure which motor will make it turn right
+           sensor = read_analog(0);
+           if(sensor > threshold_wall) {
+             set_motor(1, 30);
+             set_motor(2, 30);   
+           }
+           else {
+             sensor_right = read_analog(3);
+             while(sensor_right < threshold_turn) {
+                  set_motor(1, 30);
+                  sensor_right = read_analog(3);
+             }
+             while(sensor < threshold_turn) {
+                  set_motor(2, 30);
+                  sensor = read_analog(0);
+             }
+           }
         }
     }
     return 0;
