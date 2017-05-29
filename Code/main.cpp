f7ff756fe8cf4a23868718d9cdd80a5d2025b3d7
Lenton <lentonneale@gmail.com>
2017-05-29 23:56:08 +0000

    Added code

--- Code/main.cpp
+++ Code/main.cpp
@@ -4,72 +4,69 @@
 
 int main(){
     init();
-    char pix[32], pix_two[32]; //arrays for storing pixel values, two arrays so the robot can check two rows of pixels incase there is a problem with one
-    int nwp = 0; //counts white pixels from picture
-    int nwp2 = 0;
-    int vLeft = 70, vRight = 70; //speed variables
-    int error = 0; //error value
-    int prev_error = 0; //previous error value
-    double error_diff = 0; //difference in errors
-    int error_total = 0; //sum of all error values
-    double prop_signal = 0; //proportional signal
-    double int_signal = 0; //integral signal
-    double deriv_signal = 0; //derivative signal
-    int error2 = 0;
-    int prev_error2 = 0;
-    double error_diff2 = 0;
-    int error_total2 = 0;
-    double prop_signal2 = 0;
-    double int_signal2 = 0;
-    double deriv_signal2 = 0;
+    /*
+   set_motor(1, 60);
+   set_motor(2, 60);
+   sleep1(3,0);
+   set_motor(1, 0);
+   set_motor(2, 0);
+   return 0;
+    */
+    
+    int pix[320];                      //, pix_two[32];
+    int nwp = 0;                       //counts white pixels from picture
+    
+    int vLeft = 70, vRight = 70;           //speed variables
+    int error = 0;                         //error value
+    int prev_error = 0;                    //previous error value
+    double error_diff = 0;                 //difference in errors
+    int error_total = 0;                   //sum of all error values
+    double prop_signal = 0;                //proportional signal
+    double int_signal = 0;                 //integral signal
+    double deriv_signal = 0;              //derivative signal
+    /////////////////////////////////////////////////////////////////////////////////////////
+    
+    
     int s, x; 
-    float kp = 0.05;
-    float ki = 0.05;
-    float kd = 0.05; //values that determine the strength of the PID
+    float kp = 0.01;
+   // float ki = 0.05;
+    float kd = 0.05;
     while(true){
         take_picture();
+        //display_picture(1,0);
         nwp = 0;
-        for(int i=0; i<32; i++){
-            pix[i] = get_pixel(120, i*10, 3);
-            pix_two[i] = get_pixel(115, i*10, 3);
-            if(pix[i]>127){ 
+        error = 0;
+        for(int i=0; i<320; i++){
+            pix[i] = get_pixel(120, i, 3);
+            if(pix[i]>127){ //might want to change value that determines colour
                 s = 1;
                 nwp++;
             } else {
                 s = 0;
             }
-            if(pix_two[i]>127) {
-				x = 1;
-				nwp2++;
-			}
-			else {
-				x =0;
-			}
-            error = error + (i - 16) * s;
-            error_diff = error - prev_error;
-            error_total = error_total + error;
-            prev_error = error;
+          
+			x =0;
+			
+            error = error + (i - 160) * s;
+            //error_diff = error - prev_error;
+            //error_total = error_total + error;
+            //prev_error = error;
             prop_signal = error * kp;
-            deriv_signal = error_diff * kd;
-            int_signal = error_total * ki;
-            error2 = error2 + (i - 16) * x;
-            error_diff2 = error2 - prev_error2;
-            error_total2 = error_total2 + error2;
-            prev_error2 = error2;
-            prop_signal2 = error2 * kp;
-            deriv_signal2 = error_diff2 * kd;
-            int_signal2 = error_total2 * ki;
+            //deriv_signal = error_diff * kd;
+           // int_signal = error_total * ki;
         }
+        
+       //printf(" error=%d nwp=%d\n",error,nwp);
+ 
 
+              
         
-        
-        if(nwp>0 || nwp2>0){
-            vLeft = 35 + prop_signal + int_signal + deriv_signal;
-            vRight = 35 + prop_signal + int_signal + deriv_signal; 
-            if(nwp < nwp2) {
-				vLeft = 35 +  prop_signal1 + int_signal1 + deriv_signal1;
-				vRight = 35 + prop_signal1 + int_signal1 + deriv_signal1;
-			}         
+        if(nwp>0){
+            vLeft = 35 - prop_signal;// + int_signal + deriv_signal;
+            vRight = 35 + prop_signal;// - int_signal - deriv_signal; 
+            
+				
+			       
 			if(vLeft >= 255) {
 				vLeft = 254;
 			} 
@@ -84,16 +81,21 @@ int main(){
 			} 
             set_motor(1, vRight);
             set_motor(2, vLeft);
-        }
-        else {
-            vLeft = -70;
-            vRight = -70;
+stop(1);
+        
+       } else {
+            vLeft = -35;
+            vRight = -35;
             set_motor(1, vRight);
             set_motor(2, vLeft);
+			sleep1(0, 500000);
+			stop(0);
         }
+         
+         
         /*while(true) {
             int adc_reading = read_analog(0);
-            int threshold; placeholder variable, should remove once we know how much the value for the sensor will be when the robot is too close to the wall
+            int threshold; //placeholder variable, should remove once we know how much the value for the sensor will be when the robot is too close to the wall
             if(adc_reading <= threshold) {
                 stop(0);
                 Add the stuff to check/open the gate
@@ -107,8 +109,7 @@ int main(){
                 set_motor(2, vLeft);
                 sleep1(0, 25000);
             }
-        } This has been commented because it is not yet functional*/
+        }*/
     }
-    return 0; //And additional thing to note is that the camera may be oriented incorrectly making the error calculations unusable and could be the reason why it only moves in a circle
+    return 0;
 }
-
