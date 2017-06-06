496bda923114e439bc520fa1ed39cf449815130c
ACalzone <callumsutikno@gmail.com>
2017-06-06 12:40:42 +1200

    formatting stuff

--- Code/main.cpp
+++ Code/main.cpp
@@ -3,19 +3,17 @@
 #include "E101.h"
 
 int main(){
-    init();
+	init();
     /*
-   set_motor(1, 60);
-   set_motor(2, 60);
-   sleep1(3,0);
-   set_motor(1, 0);
-   set_motor(2, 0);
-   return 0;
-    */
-    
+   	set_motor(1, 60);
+   	set_motor(2, 60);
+   	sleep1(3,0);
+   	set_motor(1, 0);
+   	set_motor(2, 0);
+   	return 0;
+    */    
     int pix[320];                      //, pix_two[32];
     int nwp = 0;                       //counts white pixels from picture
-    
     int vLeft = 70, vRight = 70;           //speed variables
     int error = 0;                         //error value
     int prev_error = 0;                    //previous error value
@@ -24,12 +22,9 @@ int main(){
     double prop_signal = 0;                //proportional signal
     double int_signal = 0;                 //integral signal
     double deriv_signal = 0;              //derivative signal
-    /////////////////////////////////////////////////////////////////////////////////////////
-    
-    
     int s, x; 
     float kp = 0.01;
-   // float ki = 0.05;
+   	// float ki = 0.05;
     float kd = 0.05;
     while(true){
         take_picture();
@@ -43,30 +38,20 @@ int main(){
                 nwp++;
             } else {
                 s = 0;
-            }
-          
-			x =0;
-			
+            }          
+			x =0;			
             error = error + (i - 160) * s;
             //error_diff = error - prev_error;
             //error_total = error_total + error;
             //prev_error = error;
             prop_signal = error * kp;
             //deriv_signal = error_diff * kd;
-           // int_signal = error_total * ki;
-        }
-        
-       //printf(" error=%d nwp=%d\n",error,nwp);
- 
-
-              
-        
+           	// int_signal = error_total * ki;
+        }        
+       	//printf(" error=%d nwp=%d\n",error,nwp);
         if(nwp>0){
             vLeft = 35 - prop_signal;// + int_signal + deriv_signal;
-            vRight = 35 + prop_signal;// - int_signal - deriv_signal; 
-            
-				
-			       
+            vRight = 35 + prop_signal;// - int_signal - deriv_signal; 			       
 			if(vLeft >= 255) {
 				vLeft = 254;
 			} 
@@ -81,9 +66,9 @@ int main(){
 			} 
             set_motor(1, vRight);
             set_motor(2, vLeft);
-stop(1);
-        
-       } else {
+			stop(1);       
+       	} 
+		else {
             vLeft = -35;
             vRight = -35;
             set_motor(1, vRight);
@@ -91,9 +76,7 @@ stop(1);
 			sleep1(0, 500000);
 			stop(0);
         }
-         
-         
-        /*while(true) {
+		/*while(true) {
             int adc_reading = read_analog(0);
             int threshold; //placeholder variable, should remove once we know how much the value for the sensor will be when the robot is too close to the wall
             if(adc_reading <= threshold) {
